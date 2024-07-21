52ba34e78b90e13bc1f0a6e654dd9cb56f7515f4
K1ra-Python <maksimka.guchenko@mail.ru>
2024-07-21 15:44:06 +0300

    Добавление стиля и вывода коментариев

--- startComms.php
+++ startComms.php
@@ -3,25 +3,52 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
+
 <body>
-    <h1>Форма для добавления коментариев</h1>
-    <form action="vendor/addComment.php" method="post">
-        <div class="btnWrap">
-            <input type="text" name="nameUser" placeholder="Введите ваше имя"/>
-            <label for="nameUser">
-                Ваше имя
-            </label>
-        </div>
-        <div class="btnWrap">
-            <textarea name="comment">
+    <div class="wrapForma">
+        <h1>Форма для добавления коментариев</h1>
+        <form action="vendor/addComment.php" method="post">
+            <div class="btnWrap">
+                <input type="text" name="nameUser" placeholder="Введите ваше имя" />
+                <label for="nameUser">
+                    Ваше имя
+                </label>
+            </div>
+            <div class="btnWrap">
+                <textarea name="comment" rows="8" cols="40">
             </textarea>
-        </div>
-        <button type="submit">Отправить коментарий</button>
-    </form>
+            </div>
+            <button type="submit">Отправить коментарий</button>
+        </form>
+    </div>
+    <div class="wrapComments">
+        <?php
+        require_once "vendor/connect.php";
+
+        $sqlQ = "SELECT * FROM comms";
+        $stmt = $dbh->prepare($sqlQ);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($result as $row) {
+            echo '<div class="com">';
+            echo '<div class="nameUserComment">';
+            echo $row['name'];
+            echo '</div>';
+            echo '<div class="textUserComment">';
+            echo $row['text'];
+            echo '</div>';
+            //var_dump($row);
+            echo '</div>';
+        }
+        ?>
+    </div>
 </body>
+
 </html>
